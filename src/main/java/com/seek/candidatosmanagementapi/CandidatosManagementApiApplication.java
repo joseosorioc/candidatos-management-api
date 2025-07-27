16e0e54fb6e378c603aae087a179ff75d0e5e605
joseosorioc <jossekarlos10@gmail.com>
2025-07-27 14:47:00 -0500

    first commit

---
+++ src/main/java/com/seek/candidatosmanagementapi/CandidatosManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.seek.candidatosmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatosManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidatosManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seek/candidatosmanagementapi/CandidatosManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seek.candidatosmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatosManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
